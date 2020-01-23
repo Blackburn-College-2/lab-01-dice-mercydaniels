a9d4e387bc81967f31c8ee11d7765fc2da0013e8
mercydaniels <mercy.daniels@blackburn.edu>
2020-01-23 13:46:19 -0600

    dice class done ??

---
+++ src/emptyjavaproject/Dice.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+package emptyjavaproject;
+
+/**
+ *
+ * @author mercy.daniels
+ */
+public class Dice {
+  private int side;
+  
+  public int roll(){
+    int[] diceSides = {1,2,3,4,5,6};
+    int i = (int) (Math.random()*5) + 1;
+    
+      return 1;
+      
+  }
+}
